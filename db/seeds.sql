fd3dd66643317c17d7a074892a128e88bcbd5788
Brahm V <brahmvanh@gmail.com>
2023-02-27 17:22:43 -0500

    Little more. Changed schema file a little.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,13 @@
-INSERT INTO roles (id, title, salary)
-VALUES (001, "General Manager", 343200),    
-       (002, "Sales Manager", 86573), 
-       (003, "Building Manager", 85367), 
-       (004, "Grounds Manager", 40805), 
-       (005, "Lead Engineer", 88307),  
-       (006, "Software Engineer", 74344),
-       (007, "Graphic Designer", 102800),
-       (008, "Salesperson", 65465),
-       (009, "Accountant", 55469);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("General Manager", 343200, 007),    
+       ("Sales Manager", 86573, 007), 
+       ("Building Manager", 85367, 007), 
+       ("Grounds Manager", 40805, 007), 
+       ("Lead Engineer", 88307, 001),  
+       ("Software Engineer", 74344, 001),
+       ("Graphic Designer", 102800, 006),
+       ("Salesperson", 65465, 003),
+       ("Accountant", 55469, 002);
 
 INSERT INTO departments (id, department_name)
 VALUES  (001, "Engineering"),
@@ -19,18 +19,14 @@ VALUES  (001, "Engineering"),
         (007, "Management");
         
 
-INSERT INTO employees (id, first_name, last_name, role_id, manager_)
-VALUES (001, "Tim", "Johnson", "Grounds Manager", 40805, "Han"),
-       (002, "Han", "Mosquite", "Building Manager", 85367, "Thor"),
-       (003, "Arturo", "Moreno", "Lead Engineer", 88307, "Lucas"),
-       (004, "Jared", "Furomsubweh", "Software Engineer", 74344, "Arturo"),
-       (005, "Janine", "Harrisburg", "Software Engineer", 74344, "Arturo"),
-       (006, "Clementine", "McLovin", "Graphic Designer", 102800, null),
-       (007 "Moraine", "Van Heusen", "Salesperson", 65465, "Joe"),
-       (008, "Orlando", "Blemheuber", "Salesperson", 65465, "Lucas"),
-       (009, "Thor", "Odinson", "General Manager", 343200, null),
-       (010, "Joe", "Ocumalvelea", "Sales Manager", 86573, "Thor"),
-       (011, "Jackson", "Heffenstemph", "Accountant", 55469, "Lucas");
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
+VALUES ("Tim", "Johnson", 002, 004),
+       ("Han", "Mosquite", 0007, 003),
+       ("Arturo", "Moreno", null 005),
+       ("Jared", "Furomsubweh", 003, 006),
+       ("Janine", "Harrisburg", 003, 006),
+       ("Clementine", "McLovin", null, 007),
+       ("Thor", "Odinson", null, 001);
 
 
 
