8d52b65af8573a94592fa195f7e5ea5890cf4d3c
Brahm V <brahmvanh@gmail.com>
2023-02-27 21:17:26 -0500

    Falling on my sword.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 // get the client
 const mysql = require('mysql2');
+const cTable = require('console.table');
 
 // create the connection
 const connection = mysql.createConnection(
@@ -57,7 +58,7 @@ function promptUser() {
                 break;
 
                 case "Add a department":
-                    addADepartment();
+                    addDepartment();
                 break;
 
                 case "Add a role":
@@ -80,24 +81,110 @@ function promptUser() {
     };
 
 
-const viewAllDepartments = () => {
+var roleArray = [];
+
+const selectRole = () => {
+    connection.query("SELECT * FROM role", function(err, res) {
+        if (err) {
+            console.log(err);
+        }
+        dbRoles = res;
+        for (role of dbRoles) {
+            roleArray.push(role.title)
+            console.log(roleArray);
+        }
+    });
+    return roleArray;
+};
+
+var departmentsArray = [];
+const selectDepartments = () => {
+    connection.query("SELECT * FROM departments", function(err, res) {
+        if (err) {
+            console.log(err);
+        }
+        dbDepartments = res;
+        for (department of dbDepartments) {
+            departmentsArray.push(department.title);
+        }
+    });
+    return departmentsArray;
+}
+
+var employeeArray = [];
+
+const selectEmployees = () => {
+    connection.query("SELECT * FROM employees", function(err, res) {
+        if(err) {
+            console.log(err);
+        }
+        dbEmployees = res;
+        for (employee of dbEmployees) {
+            employeeArray.push(employee.title)
+        }
+    });
+    return employeeArray;
+}
+
+    const viewAllDepartments = () => {
+        connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;", 
+        function(err, res) {
+            if (err) { 
+                console.log(err);
+            }
+            cTable(res)
+            promptUser();
+  })
 
 }
 
 const viewAllRoles = () => {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;",
+    function(err, res) {
+        if (err) {
+            console.log(err);
+        }
+        cTable(res);
+         promptUser()
+    });
+};
 
-}
+const viewAllEmployees = () => {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
+    function(err, res) {
+        if(err) {
+            console.log(err);
+        }
+        cTable(res);
+        promptUser();
+    });
+};
 
 const addDepartment = () => {
-
-}
+    return inquirer.prompt([
+        {
+            type: 'Input',
+            message: 'Choose a license',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
+            name: 'promptNewDepartment',
+            validate: userInput => {
+                if (userInput) {
+                    return true;
+                } if(!userInput) {
+                    console.log("Please select an option.");
+                }
+            }
+                
+        }
+    ]);
+};
 
 const addRole = () => {
 
 }
 
 const addAnEmployee = () => {
-
+    
 }
 
 const updateEmployeeRole = () => {
